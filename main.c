ebae0599ecf4e45c751cfa080aa5b0131a58d013
Sean Allen <angstyloop@gmail.com>
2023-03-13 00:15:20 -0700

    Added a filechooser with a "Save Options..." button, which opens the custom GtkExample widget (a dialog hooked up to GSettings).

--- main.c
+++ main.c
@@ -7,6 +7,28 @@ static int app_flags = G_APPLICATION_DEFAULT_FLAGS;
 static int app_flags = G_APPLICATION_FLAGS_NONE;
 #endif
 
+#define GTK_RESPONSE_EXAMPLE 0
+
+static void
+gtk_file_chooser_response( GtkFileChooserDialog *chooser,
+	gint response_id, gpointer user_data )
+{
+	if (response_id == GTK_RESPONSE_ACCEPT) {
+		GFile *file = gtk_file_chooser_get_file( GTK_FILE_CHOOSER( chooser ) );
+		gchar* path = g_file_get_path(file);
+		puts(path);
+		g_free(path);
+		g_object_unref(file);
+		g_object_unref(chooser);
+	} else if (response_id == GTK_RESPONSE_EXAMPLE) {
+		GtkExample* e = GTK_EXAMPLE( user_data );
+		gtk_window_present( GTK_WINDOW( e ) );
+	} else {
+		/* ... */
+	}
+
+}
+
 static void
 gtk_example_response( GtkDialog *example,
 	gint response_id, gpointer user_data )
@@ -19,18 +41,32 @@ static void
 activate( GtkApplication *app, gpointer user_data )
 {
 	GtkWidget *window = gtk_application_window_new( app );
+
+	GtkWidget* chooser =
+		gtk_file_chooser_dialog_new("Open File",
+			(GtkWindow*) window,
+			GTK_FILE_CHOOSER_ACTION_OPEN,
+			"Cancel",
+			GTK_RESPONSE_CANCEL,
+			"Open",
+			GTK_RESPONSE_ACCEPT,
+			NULL);
+
 	GtkWidget *e = ( GtkWidget* ) gtk_example_new();
-	gtk_window_set_transient_for( GTK_WINDOW( e ), GTK_WINDOW( window ) );
-	gtk_window_present( GTK_WINDOW ( e ) );
-	GtkWidget *box = gtk_box_new( GTK_ORIENTATION_VERTICAL, 0 );
-	gtk_widget_set_halign ( box, GTK_ALIGN_CENTER );
-	gtk_widget_set_valign ( box, GTK_ALIGN_CENTER );
-	gtk_window_set_child ( GTK_WINDOW ( window ), box );
+
+	gtk_dialog_add_button( GTK_DIALOG( chooser ), "Save Options...", GTK_RESPONSE_EXAMPLE );
+
+	g_signal_connect( chooser,
+		"response", G_CALLBACK(gtk_file_chooser_response), e );
+
 	gtk_window_set_transient_for( GTK_WINDOW( e ), GTK_WINDOW( window ));
+
 	g_signal_connect( GTK_DIALOG( e ),
 		"response", G_CALLBACK( gtk_example_response ), window );
+
 	gtk_widget_show( window );
-	gtk_window_present( GTK_WINDOW( e ) );
+
+	gtk_widget_show(chooser);
 }
 
 int
